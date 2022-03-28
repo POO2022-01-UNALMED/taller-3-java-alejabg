610fa84a76f06de27aa64108c2e50c5a8cb84654
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-03-28 13:34:28 -0500

    .

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,103 @@
+package taller3.televisores;
+
+public class TV {
+
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    public Control control;
+    private static int numTV;
+
+    public TV(Marca marca, boolean estado){
+        this.marca = marca;
+        this.estado = estado;
+        this.numTV++;
+    }
+
+    public static int getNumTV(){
+        return numTV;
+    }
+    public static void setNumTV(int numTV){
+        TV.numTV = numTV;
+    }
+
+    public Marca getMarca(){
+        return marca;
+    }
+    public void setMarca(Marca marca){
+        this.marca = marca;
+    }
+
+    public Control getControl(){
+        return control;
+    }
+    public void setControl(Control control){
+        this.control = control;
+    }
+    
+    public int getPrecio(){
+        return precio;
+    }
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+
+    public int getVolumen(){
+        return volumen;
+    }
+    public void setVolumen(int volumen){
+        this.volumen = volumen;
+    }
+
+    public int getCanal(){
+        return canal;
+    }
+    public void setCanal(int canal){
+        this.canal = canal;
+    }
+
+    public void turnOn(){
+        this.estado = true;
+    }  
+    public void turnOff(){
+        this.estado = false;
+    }
+
+    public boolean getEstado(){
+        return estado;
+    }
+
+    public void canalUp(){
+        if(this.estado == true && this.canal >= 1 && this.canal < 120){
+            this.canal++;
+        }else{
+            return;
+        }
+    }
+    public void canalDown(){
+        if(this.estado == true && this.canal > 1 && this.canal <= 120){
+            this.canal--;
+        }else{
+            return;
+        }
+    }
+
+    public void volumenUp(){
+        if(this.estado == true && this.volumen >= 0 && this.volumen < 7){
+            this.volumen++;
+        }else{
+            return;
+        }
+        
+    }
+    public void volumenDown(){
+        if(this.estado == true && this.volumen > 0 && this.volumen <= 120){
+            this.volumen--;
+        }else{
+            return;
+        }
+    }
+
+}
